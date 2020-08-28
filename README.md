1ca73930a4a8f983e08b9773a0c67f62aa4d1a2e
swapnil <agarwal.swapnil640@gmail.com>
2020-08-29 00:54:45 +0530

    Make some changes

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Assignment_8
+
 Python Basics
+
+## References
+
+https://docs.python.org/2/library/string.html
+https://www.geeksforgeeks.org/python-using-2d-arrays-lists-the-right-way/
+https://link.springer.com/chapter/10.1007/978-3-319-08783-2_46
+https://www.geeksforgeeks.org/floor-ceil-function-python/
+https://www.codesdope.com/python-readreadread/
+https://en.wikibooks.org/wiki/Python_Programming/Operators
+
--- ps2.py
+++ ps2.py
@@ -1,1 +1,82 @@
+'''
+### ==================================================================== ###
+###                      Import Libraries                                ###
+### ==================================================================== ###
+import math
+'''
 
+### ==================================================================== ###
+###                      Function Definitions                            ###
+### ==================================================================== ###
+def prompt(turn):
+    print ("Player "+ str(turn) +"\'s chance: ")
+    print("Enter the position and number to be entered: ")
+    pos = int(input())
+    num = int(input())
+    return pos, num
+
+
+def convertPosToXY(pos, ncols):
+    pos = pos - 1 # subtract one from pos
+    x = pos // ncols
+    y = pos - ncols * x
+    return x,y
+    
+def validateNum(num, turn, n,l):
+    # check if num has been already used for this user
+    if num in l:
+        msg = "Error: Number already present on board!!\n"
+        return False,msg
+    if (turn == 1):
+        if (num % 2 != 0):
+            if (num > n*n):
+                msg = "Error: You have entered the number greater than " + str(n*n)
+                return False, msg
+        else:
+            msg = "Error: You have entered an even no!!\n"
+            return False, msg
+    elif turn == 2:
+        if (num % 2 == 0):
+            if(num > n*n):
+                return False, msg
+        else:
+            msg = "Error: You have entered odd no.!!\n"
+            return False, msg
+    msg = ""
+    return True, msg
+
+### ==================================================================== ###
+###                 Execution starts here                                ###
+### ==================================================================== ###
+if __name__ == "__main__":
+    print("Welcome to the Game!")
+    over = False
+    turn = 0
+    cnt = 0
+    # Create 2D array and initialize its elements to 0
+    nrows,ncols = 3,3
+    arr = [[0]*ncols]*nrows
+    l = []
+    while(over == False):
+        pos, num = prompt(turn+1)
+        flag, msg = validateNum(num, turn+1, nrows, l)
+
+        if (flag == False):
+            print(msg)
+            continue
+
+        x,y = convertPosToXY(pos, ncols)
+        if (arr[x][y] != 0):
+            print("Invalid entry!! element already present at this location!!\n")
+            continue
+        l.append(num)
+        arr[x][y] = num
+        turn = (turn + 1) % 2
+        print(pos, num)
+        cnt += 1
+        if (cnt == 9):
+            over = True
+        for i in arr:
+            for item in i:
+                print(item,end=' ')
+            print()
